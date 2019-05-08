8466046b3b35e13946274917e119986c562ce353
Ralph Wiest <lioncrazed@gmail.com>
2019-05-08 19:15:38 -0400

    added nodemon.json file

--- backend/controllers/action-controller.js
+++ backend/controllers/action-controller.js
@@ -1,4 +1,4 @@
 // initialize Cloudant
 const Cloudant = require('@cloudant/cloudant');
-// const cloudant = new Cloudant({ url: process.env.DB_URL, plugins: 'promises' });
+const cloudant = new Cloudant({ url: process.env.DB_URL, plugins: 'promises' });
 
--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -5,5 +5,5 @@ const BLUE_PAGE_LDAP_ORG = 'o=ibm.com';
 
 // initialize Cloudant
 const Cloudant = require('@cloudant/cloudant');
-// const cloudant = new Cloudant({ url: process.env.DB_URL, plugins: 'promises' });
+const cloudant = new Cloudant({ url: process.env.DB_URL, plugins: 'promises' });
 
