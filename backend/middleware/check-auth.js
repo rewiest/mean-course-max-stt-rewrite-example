1565357ebbf22c309c8582414efcdd173592efaf
Ralph Wiest <lioncrazed@gmail.com>
2019-05-08 18:34:59 -0400

    a

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -5,12 +5,12 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.JWT_KEY);
     req.userData = {
-      // email: decodedToken.email,
-      // firstName: decodedToken.firstName,
-      // lastName: decodedToken.lastName,
-      // preferredName: decodedToken.preferredName,
-      // roles: decodedToken.roles,
-      // _id: decodedToken._id
+      email: decodedToken.email,
+      firstName: decodedToken.firstName,
+      lastName: decodedToken.lastName,
+      preferredName: decodedToken.preferredName,
+      roles: decodedToken.roles,
+      _id: decodedToken._id
     };
     next();
   } catch (error) {
