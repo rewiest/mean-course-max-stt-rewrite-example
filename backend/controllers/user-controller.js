66480d43aa4b53efbdd640abb93ba8434fdbff9e
Ralph Wiest <lioncrazed@gmail.com>
2019-05-08 21:50:55 -0400

    added user db creation

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -7,3 +7,26 @@ const BLUE_PAGE_LDAP_ORG = 'o=ibm.com';
 const Cloudant = require('@cloudant/cloudant');
 const cloudant = new Cloudant({ url: process.env.DB_URL, plugins: 'promises' });
 
+// create Users database if it does not already exist
+cloudant.db.create('users')
+  .then((data) => {
+    console.log('Users database created');
+  })
+  .catch((error) => {
+    if (error.error === 'file_exists') {
+      console.log('Users database already exists');
+    } else {
+      console.log('Error occurred when creating Users database', error.error);
+    }
+  });
+  const usersDb = cloudant.db.use('users');
+
+// create Users database index for email
+var email = {name:'email', type:'json', index:{fields:['email']}}
+usersDb.index({name:'email', type:'json', index:{fields:['email']}})
+  .then((response) => {
+    console.log('Users database Email index -', response.result);
+  })
+  .catch((error) => {
+    console.log('Error occurred when creating Users database Email index', error.error);
+  });
